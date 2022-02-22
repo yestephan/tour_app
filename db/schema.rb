e980305b8cde2c9e7a265f31039c6126f23e13a5
Tzuning Chiu <chiutzuning@gmail.com>
2022-02-22 12:42:40 +0100

    remove to tour

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_22_113153) do
+ActiveRecord::Schema.define(version: 2022_02_22_114042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2022_02_22_113153) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "title"
-    t.string "name"
     t.index ["user_id"], name: "index_tours_on_user_id"
   end
 
