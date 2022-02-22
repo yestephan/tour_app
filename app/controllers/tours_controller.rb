da2507154fe3b9f142ef1afc7ae45586b4eddd5a
Tzuning Chiu <chiutzuning@gmail.com>
2022-02-22 21:52:51 +0100

    add some layout in tour/index

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,5 +1,5 @@
 class ToursController < ApplicationController
-  before_action :set_list, only: [:show, :edit, :update, :destroy]
+  before_action :set_tour, only: [:show, :edit, :update, :destroy]
   def index
     @tours = Tour.all
   end
